3640a5b7b73d37d1ceb83d90eb47d6d0aa1419d5
Daniel Hou <danielhou315@gmail.com>
2024-04-27 15:29:14 -0400

    Added obb loss

--- yolo_project/loss_obb.py
+++ yolo_project/loss_obb.py
@@ -5,26 +5,23 @@ Implementation of Yolo Loss Function from the original yolo paper
 
 import torch
 import torch.nn as nn
-from utils import intersection_over_union
+from utils import oriented_iou
 
 
-class YoloOBBLoss(YoloLoss):
+class YoloOBBLoss(nn.Module):
     """
     Calculate the loss for yolo (v1) model
     """
-
     def __init__(self, S=7, B=2, C=20):
         super().__init__()
-
-    
 
     def forward(self, predictions, target):
         # predictions are shaped (BATCH_SIZE, S*S(C+B*5) when inputted
         predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B * 5)
 
         # Calculate IoU for the two predicted bounding boxes with target bbox
-        iou_b1 = intersection_over_union(predictions[..., 21:25], target[..., 21:25])
-        iou_b2 = intersection_over_union(predictions[..., 26:30], target[..., 21:25])
+        iou_b1,_,_,_ = oriented_box_iou(predictions[..., 21:26], target[..., 21:26])
+        iou_b2,_,_,_ = oriented_box_iou(predictions[..., 27:32], target[..., 27:32])
         ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
 
         # Take the box with highest IoU out of the two prediction
@@ -40,12 +37,12 @@ class YoloOBBLoss(YoloLoss):
         # predictions, which is the one with highest Iou calculated previously.
         box_predictions = exists_box * (
             (
-                bestbox * predictions[..., 26:30]
-                + (1 - bestbox) * predictions[..., 21:25]
+                bestbox * predictions[..., 27:32]
+                + (1 - bestbox) * predictions[..., 21:26]
             )
         )
 
-        box_targets = exists_box * target[..., 21:25]
+        box_targets = exists_box * target[..., 21:26]
 
         # Take sqrt of width, height of boxes to ensure that
         box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(
@@ -64,7 +61,7 @@ class YoloOBBLoss(YoloLoss):
 
         # pred_box is the confidence score for the bbox with highest IoU
         pred_box = (
-            bestbox * predictions[..., 25:26] + (1 - bestbox) * predictions[..., 20:21]
+            bestbox * predictions[..., 26:27] + (1 - bestbox) * predictions[..., 20:21]
         )
 
         object_loss = self.mse(
@@ -88,8 +85,8 @@ class YoloOBBLoss(YoloLoss):
         )
 
         no_object_loss += self.mse(
-            torch.flatten((1 - exists_box) * predictions[..., 25:26], start_dim=1),
-            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1)
+            torch.flatten((1 - exists_box) * predictions[..., 26:27], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., 26:27], start_dim=1)
         )
 
         # ================== #
@@ -101,23 +98,22 @@ class YoloOBBLoss(YoloLoss):
             torch.flatten(exists_box * target[..., :20], end_dim=-2,),
         )
 
+        # ======================== #
+        #   FOR ORIENTATION LOSS   #
+        # ======================== #
+        # From complex yolo https://arxiv.org/abs/1803.06199
+        euler_loss = self.mse(
+            torch.flatten(exists_box * predictions[..., [25,31]], start_dim=1),
+            torch.flatten(exists_box * target[..., [25,31], start_dim=1),
+        )
+        
+
         loss = (
             self.lambda_coord * box_loss  # first two rows in paper
             + object_loss  # third row in paper
             + self.lambda_noobj * no_object_loss  # forth row
             + class_loss  # fifth row
+            + euler_loss
         )
 
         return loss
-
-
-if __name__ == "__main__":
-    # Test corners
-    # Example usage:
-    x, y = 0.0, 0.0
-    h, w = 2.0, 4.0
-    theta = torch.tensor(torch.pi / 4)  # 45 degrees in radians
-    
-    corners = compute_oriented_bbox_corners(x, y, h, w, theta)
-    print("Corners of the oriented bounding box:")
-    print(corners)
